c2b740c7c62ffbbf22ca2e67f2b3e0eb8df9c2c3
Katherine <mejiakatherine7@gmail.com>
2024-05-15 20:00:54 -0400

    renamed some css files, merged result and gif js into one js file

--- assets/result.js
+++
@@ -1,8 +0,0 @@
-const backButton = document.getElementById('backButton');
-
-backButton.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    window.location.href = 'index.html';
-
-});
